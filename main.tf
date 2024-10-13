b0a5c19108d1d75f98865830fd268df1fbcd43ba
akshaykatkam022 <akshaykatkam022@gmail.com>
2024-10-13 14:30:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,19 @@ resource "google_storage_bucket" "my-bucket" {
   force_destroy            = true
   public_access_prevention = "enforced"
 }
+resource "google_compute_instance" "default" {
+  name         = "firstgcp-instance"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
