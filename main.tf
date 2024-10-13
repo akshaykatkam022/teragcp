33c72f01cb0474317e07e8e399372c9908234c4d
akshaykatkam022 <akshaykatkam022@gmail.com>
2024-10-13 20:32:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,12 @@ resource "google_storage_bucket" "my-bucket" {
   name                     = "akshay-devops-gcp-terraform"
   project                  = "test-terraform-438504"
   location                 = "US"
-  force_destroy            = true
-  public_access_prevention = "enforced"
+  
   
 }
 resource "google_storage_bucket" "my-bucket2" {
   name                     = "import-test-bucket-terarform"
   project                  = "test-terraform-438504"
   location                 = "US"
-  force_destroy            = true
-  public_access_prevention = "enforced"
+  
 }
