1af9d75e6e5b0e515a753d86005e24e54a2edd46
akshaykatkam022 <akshaykatkam022@gmail.com>
2024-10-13 19:27:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,20 +4,12 @@ resource "google_storage_bucket" "my-bucket" {
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
+   depends_on = google_storage_bucket.my firstbucket
 }
-resource "google_compute_instance" "default" {
-  name         = "firstgcp-instance"
-  machine_type = "e2-micro"
-  zone         = "us-central1-a"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    network = "default"
-    access_config {}
-  }
+resource "google_storage_bucket" "my-bucket2" {
+  name                     = "akshay2-devops-gcp-terraform"
+  project                  = "test-terraform-438504"
+  location                 = "US"
+  force_destroy            = true
+  public_access_prevention = "enforced"
 }
