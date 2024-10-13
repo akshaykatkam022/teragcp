1152762e1468f76308caebd70d987b3f7e0de57c
akshaykatkam022 <akshaykatkam022@gmail.com>
2024-10-13 14:03:27 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "google_storage_bucket" "my-bucket" {
+  name                     = "akshay-devops-gcp-terraform"
+  project                  = "test-terraform-438504"
+  location                 = "US"
+  force_destroy            = true
+  public_access_prevention = "enforced"
+}
